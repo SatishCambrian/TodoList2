39bdf3ff0a90325c8839e64a1dd94c78ccfdfbc6
SatishCambrian <SatishCambrian@users.noreply.github.com>
2024-02-14 18:46:36 -0500

    small fix

--- TodoList.API/obj/Debug/net8.0/TodoList.API.AssemblyInfo.cs
+++ TodoList.API/obj/Debug/net8.0/TodoList.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoList.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aed7d8d07013e18c78a7cacb450079cd3d1b0a3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98eb12842f726c79cd7824f72b19b40c562b7dd5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoList.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoList.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
