73507d9f31c6efdbdd7397692ed302144b7f9ce4
SatishCambrian <SatishCambrian@users.noreply.github.com>
2024-02-14 17:38:19 -0500

    Created a Database Context in the API project

---
+++ TodoList.API/Data/TodoDbContext.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using TodoListModelClassLibrary;
+using System.Collections.Generic;
+
+namespace TodoListAPI.Data
+{
+    public class TodoDbContext : DbContext
+    {
+        // create a db context for the database using ToDoListClassLibrary
+
+        public TodoDbContext() { }
+
+        // Reference for database migration
+        public TodoDbContext(DbContextOptions<TodoDbContext> options) : base(options) => Database.Migrate();
+
+        public DbSet<ToDoItem> ToDoItems { get; set; }
+        // The following configures EF to create a Sqlite database file in the
+        // special "local" folder for your platform.
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Data Source=TodoList.db");
+    }
+
+}
